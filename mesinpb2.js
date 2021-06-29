ed615691de360c6251582ff4d12ad5a1f30838d4
okaoka_520 <ciciokta819@gmail.com>
2021-06-29 23:14:43 +0700

    Menambahkan file mesinpb2.js

---
+++ mesinpb2.js
@@ -0,0 +1,11 @@
+var jmlmesin = 20;
+var mesinbr = 16;
+var mp = 1;
+
+while(mp <= jmlmesin){
+    console.log "Mesin No "+ mp +" beroperasi dengan baik";
+    mp++;
+}
+for(mp = mesinbr +=1;mp <= jmlmesin;mp++){
+    console.log "Mesin No "+ mp +" sedang tidak beroperasi";
+}
