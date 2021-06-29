ef3d31011e737341d4eef08e762b31d6035a1fd4
okaoka_520 <ciciokta819@gmail.com>
2021-06-29 23:03:44 +0700

    Membuat file mesinpb.js

---
+++ mesinpb.js
@@ -0,0 +1,10 @@
+var jmlmasin = 20;
+var a = 1;
+while(a <= jmlmesin){
+    console.log("Mesin No. "+ a +" beroperasi dengan baik.");
+    a++;
+}
+// var a = nilai awal
+// while(kondisi terminasi)
+// console.log = isi yang ingin ditampilkan/output
+// a++ = incremen  or decremen
