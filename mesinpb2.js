0eecddcc4d67ba983bb69d26a8432d05e339ca4a
okaoka_520 <ciciokta819@gmail.com>
2021-06-30 16:52:23 +0700

    Memperbaiki kesalahan source

--- mesinpb2.js
+++ mesinpb2.js
@@ -3,9 +3,9 @@ var mesinbr = 16;
 var mp = 1;
 
 while(mp <= jmlmesin){
-    console.log "Mesin No "+ mp +" beroperasi dengan baik";
+    console.log ("Mesin No "+ mp +" beroperasi dengan baik");
     mp++;
 }
 for(mp = mesinbr +=1;mp <= jmlmesin;mp++){
-    console.log "Mesin No "+ mp +" sedang tidak beroperasi";
+    console.log ("Mesin No "+ mp +" sedang tidak beroperasi");
 }
