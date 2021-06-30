21fe803d2ee64456ac30848a8071daf007778277
okaoka_520 <ciciokta819@gmail.com>
2021-06-30 16:54:36 +0700

    Menambahkan file mesinpb3.js di ifelse

---
+++ mesinpb3t.js
@@ -0,0 +1,13 @@
+var jmlmesin = 20;
+var mesinbr = 16;
+var mp = 1;
+
+
+for (mp;mp <= jmlmesin;mp++){
+    
+    if(mp <= mesinbr){
+        console.log ("Mesin No "+ mp +" beroperasi dengan baik");
+    }else{
+        console.log ("Mesin No "+ mp +" sedang tidak beroperasi");
+    }
+}
